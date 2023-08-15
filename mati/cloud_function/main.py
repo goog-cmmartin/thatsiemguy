13b3d752158ebaa1b25cc655bb839d24eceb9c08
Christopher Martin <cmmartin@google.com>
2023-08-15 12:59:04 +0200

    initial checkin

---
+++ mati/cloud_function/main.py
@@ -0,0 +1,378 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""Fetch data from MATI API."""
+
+from typing import Optional
+
+import datetime
+import requests
+import json
+
+from common import env_constants
+from common import status
+from common import utils
+
+# Environment variables
+ENV_CHRONICLE_CUSTOMER_ID = "CHRONICLE_CUSTOMER_ID"
+ENV_CHRONICLE_REGION = "CHRONICLE_REGION"
+ENV_CHRONICLE_SERVICE_ACCOUNT = "CHRONICLE_SERVICE_ACCOUNT"
+ENV_MATI_KEY_ID = "MATI_KEY_ID"
+ENV_MATI_SECRET = "MATI_SECRET"
+ENV_APP_NAME = "APP_NAME"
+ENV_GTE_SCORE = "GTE_SCORE"
+ENV_EXPIRY = "EXPIRY"
+
+customer_id = utils.get_env_var(ENV_CHRONICLE_CUSTOMER_ID)
+region = utils.get_env_var(ENV_CHRONICLE_REGION)
+credentials_file = utils.get_env_var(ENV_CHRONICLE_SERVICE_ACCOUNT, is_secret=True)
+# load the secret into a dictionary for auth later on
+credentials_file = json.loads(credentials_file)
+
+secret_id = utils.get_env_var(ENV_MATI_KEY_ID, is_secret=True)
+# remove end of line characters that will error otherwise
+secret_id = secret_id.replace("\n", "").replace("\r", "")
+secret_key = utils.get_env_var(ENV_MATI_SECRET, is_secret=True)
+secret_key = secret_key.replace("\n", "").replace("\r", "")
+app_name = utils.get_env_var(ENV_APP_NAME)
+gte_score = utils.get_env_var(ENV_GTE_SCORE)
+expiry = utils.get_env_var(ENV_EXPIRY)
+
+
+# Generate Access Token
+def generate_bearer_token(secret_id,secret_key,app_name):
+
+  url = "https://api.intelligence.mandiant.com/token"
+  headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json",
+      "X-App-Name": "MATI to Chronicle",
+  }
+
+  data = {
+      "grant_type": "client_credentials",
+  }
+
+  auth = requests.auth.HTTPBasicAuth(secret_id, secret_key)
+
+  response = requests.post(url, headers=headers, data=data, auth=auth)
+
+  if response.status_code == 200:
+      return response
+  else:
+      return response.status_code
+
+def generate_epoch_timestamp(offset_minutes):
+  """Generates an epoch timestamp.
+
+  Args:
+    offset_minutes: The number of minutes to offset the epoch timestamp.
+
+  Returns:
+    A float representing the number of seconds since the Unix epoch.
+  """
+  from time import time
+  return int(time() - offset_minutes * 6)
+
+def instance_region(region):
+  # note, new regions will need to be added here
+  print(f"the region passed to the region function was {region}")
+  REGIONS = {
+      "europe": "https://europe-malachiteingestion-pa.googleapis.com",
+      "asia": "https://asia-southeast1-malachiteingestion-pa.googleapis.com",
+      "us": "https://malachiteingestion-pa.googleapis.com",
+  }
+  if region not in REGIONS:
+      raise ValueError("Invalid region")
+  return str(REGIONS[region])
+
+
+def get_indicators_first_page(access_token: str, start_epoch: int, gte_score: int):
+
+  url = "https://api.intelligence.mandiant.com/v4/indicator"
+  headers = {
+      "Authorization": "Bearer {}".format(access_token),
+      "Accept": "application/json",
+      "X-App-Name": "MATI to Chronicle",
+  }
+
+  params = {
+      "start_epoch": start_epoch,
+      "gte_mscore": gte_score,
+      "source": "mandiant",
+      "limit": 1000
+  }
+
+  response = requests.get(url, headers=headers, params=params)
+
+  if response.status_code == 200:
+      return response
+  else:
+      return response.status_code
+
+def get_indicators_next_page(access_token: str, next_page: str):
+  import requests
+
+  url = "https://api.intelligence.mandiant.com/v4/indicator"
+  headers = {
+      "Authorization": "Bearer {}".format(access_token),
+      "Accept": "application/json",
+      "X-App-Name": "MATI to Chronicle",
+  }
+
+  params = {
+      "next": next_page
+    }
+
+  response = requests.get(url, headers=headers, params=params)
+
+  if response.status_code == 200:
+      return response
+  elif response.status_code == 204:
+      return response
+  else:
+      return response
+
+
+def auth(credentials):
+  from google.auth.transport import requests
+  from google.oauth2 import service_account
+
+  AUTHORIZATION_SCOPES = ["https://www.googleapis.com/auth/chronicle-backstory","https://www.googleapis.com/auth/malachite-ingestion"]
+
+  credentials = service_account.Credentials.from_service_account_info(
+        credentials,
+        scopes=AUTHORIZATION_SCOPES)
+  return requests.AuthorizedSession(credentials)
+
+
+def create_entity_v2(entity_json,log_type):
+
+  authenticated = auth(credentials_file)
+
+  data = {}
+  data["customer_id"] = customer_id
+  data["log_type"] = log_type
+  data["entities"] = []
+
+  data['entities'].append(json.loads(entity_json))
+
+  json_data = json.dumps(data)
+
+  #print ('\nLog to send:\n' + json_data)
+
+  http_endpoint = '{}/v2/entities:batchCreate'.format(instance_region(region))
+  headers = {'content-type': 'application/json'}
+
+  r = authenticated.post(url=http_endpoint, data=json_data, headers=headers)
+
+  print(r.text)
+  return r
+
+def now():
+
+  # Get the current time
+  current_time = datetime.datetime.now()
+
+  # Format the current time
+  formatted_time = current_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+  return formatted_time
+
+def subtract_offset(timestamp, offset):
+  """
+  Subtracts the offset from the timestamp and returns the resulting timestamp.
+
+  Args:
+    timestamp: The timestamp to subtract the offset from.
+    offset: The offset to subtract from the timestamp.
+
+  Returns:
+    The resulting timestamp.
+  """
+
+  # Convert the timestamp to a datetime object.
+  datetime_object = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+  # Convert the offset to a timedelta object.
+  timedelta_object = datetime.timedelta(days=-offset)
+
+  # Subtract the offset from the datetime object.
+  new_datetime_object = datetime_object - timedelta_object
+
+  # Convert the new datetime object to a timestamp.
+  #new_timestamp = new_datetime_object.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+  new_timestamp = new_datetime_object.isoformat(timespec='milliseconds') + "Z"
+
+  return new_timestamp
+
+def fix_url(url,replacement_string):
+  """
+  Removes specified values from a string, e.g., remove protocol from a URL
+
+  Args:
+    timestamp: the URL to update
+    replacement_string: the gsub operation you want apply on the URL
+
+  Returns:
+    The updated URL as a string.
+  """
+  import re
+  updated_url = re.sub(replacement_string, '', url)
+  return updated_url
+
+
+def send_iocs_to_chronicle(iocs):
+  events = []
+
+  for indicator in iocs['indicators']:
+    # temporary Dictionaries and Lists to build UDM Nouns
+    metadata = {}
+    file = {}
+    threat = {}
+    interval = {}
+    entity = {}
+    labels = []
+    additionals = []
+
+    # Extract top level fields
+    # >>> ADDITIONAL
+    additionals = {}
+    additionals['mscore'] = indicator['mscore']
+    # Chronicle Detection Engine does not support Bool types, so we convert to String
+    additionals['is_publishable'] = str(indicator['is_publishable'])
+    # - entity.labels
+    for misp_key, misp_value in indicator['misp'].items():
+      # only print MISP sources where the value is True
+      # - usually for Mandiant sources this is always False
+      if misp_value is True:
+        additionals[misp_key] =  str(misp_value)
+    # >>> METADATA
+    metadata['vendor_name'] = "MANDIANT_IOC"
+    metadata['product_name'] = "MANDIANT_IOC"
+    metadata['collected_timestamp'] = str(now())
+    metadata['product_entity_id'] = indicator['id']
+    # metadata.interval
+    interval['start_time'] = subtract_offset(indicator['first_seen'],-30)
+    # - add the Expiry interval to the last_seen time to age out IOCs
+    interval['end_time'] = subtract_offset(indicator['last_seen'],30)
+    # metadata.threat
+    threat['confidence_details'] = str(indicator['mscore'])
+    for source in indicator['sources']:
+      threat['category_details'] = source['source_name']
+    # >>> ENTITY
+    # - entity.type
+    match indicator['type']:
+      case "fqdn":
+        entity['hostname'] = indicator['value']
+        metadata['entity_type'] = 'DOMAIN_NAME'
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/fqdn/{}".format(indicator['value'])
+      case "ipv4":
+        entity['ip'] = indicator['value']
+        metadata['entity_type'] = 'IP_ADDRESS'
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/ipv4/{}".format(indicator['value'])
+      case "url":
+        # remove the http or https protovol from the URL as our log sources don't log this
+        sanitized_url = fix_url(indicator['value'],"^http(s)?://")
+        entity['url'] = sanitized_url
+        #entity['url'] = indicator['value']
+        metadata['entity_type'] = 'URL'
+      case "md5":
+        file['md5'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/md5/{}".format(indicator['value'])
+      case "sha1":
+        file['sha1'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/sha1/{}".format(indicator['value'])        
+      case "sha256":
+        file['sha256'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/sha256/{}".format(indicator['value'])        
+    # entity.file
+    try:
+      for hash in indicator['associated_hashes']:
+        match hash['type']:
+          case "md5":
+            file['md5'] = hash['value']
+            threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/md5/{}".format(indicator['value'])                    
+          case "sha1":
+            file['sha1'] = hash['value']
+          case "sha256":
+            file['sha256'] = hash['value']
+    except KeyError:
+      pass
+    # build the top level UDM Objects
+    metadata['threat'] = threat
+    metadata['interval'] = interval
+    #create the final UDM event
+    event = {}
+    event['metadata'] = metadata
+    event['entity'] = entity
+    event['additional'] = additionals
+    events.append(event)
+
+  if events:
+    create_entity_v2(json.dumps(events),'MANDIANT_IOC')
+  else:
+    print("No IOCs were returned during the given interval and filter criteria.")
+
+
+
+def main(req):  # pylint: disable=unused-argument
+  """Entrypoint.
+
+  Args:
+    req: Request to execute the cloud function.
+
+  Returns:
+    string: "Ingestion completed."
+  """
+
+  bearer_token = generate_bearer_token(secret_id,secret_key,app_name)
+
+  access_token = bearer_token.json()
+
+  epoch = generate_epoch_timestamp(1440) # in minutes
+
+  # get Indicators from MATI API
+  # - this is hard coded to retrieve Mandiant sources indicators only
+  # - i.e,. the open source indicators are already in Chronicle SIEM
+  mati_api_results = get_indicators_first_page(access_token=access_token['access_token'], start_epoch=epoch, gte_score=gte_score)
+
+  next_page = mati_api_results.json()
+
+  send_iocs_to_chronicle(next_page)
+
+  try:
+    if next_page['next'] is not None:
+      while True:
+        response = get_indicators_next_page(access_token=access_token['access_token'], next_page=next_page['next'])
+        next_page = response.json()
+        send_iocs_to_chronicle(next_page)
+        import time
+        time.sleep(1)
+        try:
+          if next_page['next'] is None:
+            print("no more results") # this will never actuall get here
+            break
+        except KeyError:
+          print('No more pages.')
+  except KeyError:
+    print('No more pages.')
+
+  return "Ingestion completed."
---
+++ mati/readme.md
@@ -0,0 +1,169 @@
+# Mandiant Advanced Threat Intelligence (MATI) to Chronicle SIEM Integration
+
+<br>
+
+## Overview
+
+This repo provides an example integration between MATI and Chronicle SIEM to automate IOC matching using Chronicle SIEM's Entity Graph, and YARA-L Detection Engine.  The integration is provided as a demonstration of how to connect to a 3rd party API, in this case MATI, and using Python to create a native UDM Object to be ingested directly to the Chronicle Ingestion API udm event endpoint, aka, no CBN parser required.  The integration uses several Cloud Function environment variables to provide customization for features such as:
+* MATI IOC confidenc score threshold filtering.  The default is 80 and above.
+* Aging (decay) of IOCs.  While MATI includes a Confidence Score you can configure the Expiry value to set the interval start and interval end date for when the IOC should be considered valid.
+
+The key point of this example is that you can use other programming or scripting languages to create UDM Events or UDM Entities, for example the main body of Python code that converts a MATI API response into a UDM Entity IOC Event is as follows.  An advantage of using a programming or scripting language, such as Python, is the power and flexibility mean you can format data into Chronicle SIEM's schema with a small amount of code, but also perform functions that are not possible in Parsing via CBN, e.g., date addition, or perform functions in an easier manner, e.g., parsing nested JSON.
+
+<pre>
+  for indicator in iocs['indicators']:
+    # temporary Dictionaries and Lists to build UDM Nouns
+    metadata = {}
+    file = {}
+    threat = {}
+    interval = {}
+    entity = {}
+    labels = []
+    additionals = []
+
+    # Extract top level fields
+    # >>> ADDITIONAL
+    additionals = {}
+    additionals['mscore'] = indicator['mscore']
+    # Chronicle Detection Engine does not support Bool types, so we convert to String
+    additionals['is_publishable'] = str(indicator['is_publishable'])
+    # - entity.labels
+    for misp_key, misp_value in indicator['misp'].items():
+      # only print MISP sources where the value is True
+      # - usually for Mandiant sources this is always False
+      if misp_value is True:
+        additionals[misp_key] =  str(misp_value)
+    # >>> METADATA
+    metadata['vendor_name'] = "MANDIANT_IOC"
+    metadata['product_name'] = "MANDIANT_IOC"
+    metadata['collected_timestamp'] = str(now())
+    metadata['product_entity_id'] = indicator['id']
+    # metadata.interval
+    interval['start_time'] = subtract_offset(indicator['first_seen'],-30)
+    # - add the Expiry interval to the last_seen time to age out IOCs
+    interval['end_time'] = subtract_offset(indicator['last_seen'],30)
+    # metadata.threat
+    threat['confidence_details'] = str(indicator['mscore'])
+    for source in indicator['sources']:
+      threat['category_details'] = source['source_name']
+    # >>> ENTITY
+    # - entity.type
+    match indicator['type']:
+      case "fqdn":
+        entity['hostname'] = indicator['value']
+        metadata['entity_type'] = 'DOMAIN_NAME'
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/fqdn/{}".format(indicator['value'])
+      case "ipv4":
+        entity['ip'] = indicator['value']
+        metadata['entity_type'] = 'IP_ADDRESS'
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/ipv4/{}".format(indicator['value'])
+      case "url":
+        # remove the http or https protovol from the URL as our log sources don't log this
+        sanitized_url = fix_url(indicator['value'],"^http(s)?://")
+        entity['url'] = sanitized_url
+        #entity['url'] = indicator['value']
+        metadata['entity_type'] = 'URL'
+      case "md5":
+        file['md5'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/md5/{}".format(indicator['value'])
+      case "sha1":
+        file['sha1'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/sha1/{}".format(indicator['value'])        
+      case "sha256":
+        file['sha256'] = indicator['value']
+        metadata['entity_type'] = 'FILE'
+        entity['file'] = file
+        threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/sha256/{}".format(indicator['value'])        
+    # entity.file
+    try:
+      for hash in indicator['associated_hashes']:
+        match hash['type']:
+          case "md5":
+            file['md5'] = hash['value']
+            threat['url_back_to_product'] = "https://advantage.mandiant.com/indicator/md5/{}".format(indicator['value'])                    
+          case "sha1":
+            file['sha1'] = hash['value']
+          case "sha256":
+            file['sha256'] = hash['value']
+    except KeyError:
+      pass
+    # build the top level UDM Objects
+    metadata['threat'] = threat
+    metadata['interval'] = interval
+    #create the final UDM event
+    event = {}
+    event['metadata'] = metadata
+    event['entity'] = entity
+    event['additional'] = additionals
+    events.append(event)
+
+  if events:
+    create_entity_v2(json.dumps(events),'MANDIANT_IOC')
+  else:
+    print("No IOCs were returned during the given interval and filter criteria.")
+
+</pre>
+
+
+## Pre-requisites
+
+To utilize this integration requires the following pre-requisites:
+* You are a MATI API Key and Secret
+* Your Chronicle SIEM instance region
+* Chronicle Customer GUID
+* Chronicle Ingestion API JSON Developer Service Account
+
+## Integration Notes
+
+### Create a service account for the Cloud Function
+
+<pre>
+GCP_PROJECT="<your GCP Project>"
+
+SA_NAME="sa-mati-to-chronicle"
+SA_DESCRIPTION="Export MATI IOCs to Chronicle SIEM"
+SA_DISPLAY_NAME="SA MATI to Chronicle SIEM"
+
+gcloud iam service-accounts create $SA_NAME --description="$SA_DESCRIPTION" --display-name="$SA_DISPLAY_NAME"
+
+</pre>
+
+### Create SECRETS
+
+Note, if you have an Chronicle Ingestion API Key as a secret already, re-use that.
+
+<pre>
+gcloud secrets create MATI-KEY-ID --data-file=/home/user/mati-key-id
+gcloud secrets create MATI-SECRET --data-file=/home/user/mati-secret
+gcloud secrets create CHRONICLE-INGESTION-API --data-file=/home/user/chronicle-ingestion-key.json
+</pre>
+
+### Grant Access to Secrets
+
+<pre>
+gcloud secrets add-iam-policy-binding MATI-KEY-ID --member="serviceAccount:$SA_NAME@$GCP_PROJECT.iam.gserviceaccount.com" --project=$GCP_PROJECT --role='roles/secretmanager.secretAccessor'
+</pre>
+
+### Grant service account access to run a Cloud Function
+
+<pre>
+gcloud projects add-iam-policy-binding $GCP_PROJECT --member="serviceAccount:$SA_NAME@$GCP_PROJECT.iam.gserviceaccount.com"   --role='roles/cloudfunctions.developer'
+</pre>
+
+### Edit the Cloud Function .Env file
+
+Configure `.env.yml` by replacing the values that match your environment.
+
+### Deploy the Cloud Function
+
+<pre>
+REGION="europe-west4"
+MEMORY="4096MB"
+MIN_INSTANCES="1"
+
+gcloud functions deploy mati-to-chronicle --entry-point main --trigger-http --runtime python39 --env-vars-file .env.yml --region $REGION  --memory $MEMORY --min-instances $MIN_INSTANCES
+</pre>
